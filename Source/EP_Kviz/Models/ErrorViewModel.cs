8d2322ee76fbb8728ffe2c5fac384049ce10d616
Matyáš Hudeček <hudecekmatyas2@gmail.com>
2025-09-19 17:09:06 +0200

    Started project

---
+++ Source/EP_Kviz/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EP_Kviz.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
