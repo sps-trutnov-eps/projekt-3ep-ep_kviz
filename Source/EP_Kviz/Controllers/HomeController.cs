67d9e2bbb79e13d8374f6f937c062661268c982c
Matyáš Hudeček <151260465+Skaut0071@users.noreply.github.com>
2025-10-01 13:57:31 +0200

    pokus o zobrazeni stranky s pravidly
    stale nefunguje ale blizim se

--- Source/EP_Kviz/Controllers/HomeController.cs
+++ Source/EP_Kviz/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace EP_Kviz.Controllers
             return View();
         }
 
+        public IActionResult Pravidla()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
