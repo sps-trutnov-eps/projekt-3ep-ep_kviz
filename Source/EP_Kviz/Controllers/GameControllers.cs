09cc94280031d83d5cfc5739065727b05195f8d0
Matyáš Hudeček <151260465+Skaut0071@users.noreply.github.com>
2025-10-12 20:22:17 +0200

    Vytvoření herních sesions
    hrač muze vytvořit herni sesions kde zatim zada svoje id, a klikne na mód, vygeneruje se id sesion a pripojiho

---
+++ Source/EP_Kviz/Controllers/GameControllers.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class GamesController : Controller
+{
+    public IActionResult Vyber()
+    {
+        return View();
+    }
+
+    // Metoda pro 1v1 mód
+    public IActionResult OneVOne(int pid)
+    {
+        int gameId = GenerateRandomGameId();
+        ViewBag.GameId = gameId;
+        ViewBag.PlayerId = pid;
+        ViewBag.Mode = "1v1";
+        return View("~/Views/Games/Modes/1v1.cshtml");
+    }
+
+    // Metoda pro 2v2 mód
+    public IActionResult TwoVTwo(int pid)
+    {
+        int gameId = GenerateRandomGameId();
+        ViewBag.GameId = gameId;
+        ViewBag.PlayerId = pid;
+        ViewBag.Mode = "2v2";
+        return View("~/Views/Games/Modes/2v2.cshtml");
+    }
+
+    // Metoda pro Procvičení
+    public IActionResult Procviceni(int pid)
+    {
+        int gameId = GenerateRandomGameId();
+        ViewBag.GameId = gameId;
+        ViewBag.PlayerId = pid;
+        ViewBag.Mode = "Procvičení";
+        return View("~/Views/Games/Modes/Procviceni.cshtml");
+    }
+
+    // Generování náhodného Game ID
+    private int GenerateRandomGameId()
+    {
+        var random = new Random();
+        return random.Next(100000, 999999); // 6-místné číslo
+    }
+}
