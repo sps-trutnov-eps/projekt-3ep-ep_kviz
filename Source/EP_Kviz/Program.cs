3c7b85899c5e88fff45b31e1966952378e4fdc7d
Ondřej Imlauf <156407890+imlaufo23@users.noreply.github.com>
2025-11-05 14:06:00 +0100

    Vytvoření databáze

--- Source/EP_Kviz/Program.cs
+++ Source/EP_Kviz/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddIdentity<UzivateleModel, IdentityRole>(options => {
 builder.Services.AddMemoryCache(); 
 builder.Services.AddSingleton<EP_Kviz.Services.GameManager>();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
