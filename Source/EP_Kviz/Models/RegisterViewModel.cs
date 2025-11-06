6d0242f46e23e59ff6faaccf13f00142c08bf907
Ondřej Imlauf <156407890+imlaufo23@users.noreply.github.com>
2025-11-06 17:32:47 +0100

    Fungující registrace uživatele do databáze
    Registrační stránka nemá css

--- Source/EP_Kviz/Models/RegisterViewModel.cs
+++ Source/EP_Kviz/Models/RegisterViewModel.cs
@@ -1,6 +1,24 @@
-public class RegisterViewModel
+using System.ComponentModel.DataAnnotations;
+
+namespace EP_Kviz.Models
 {
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string Email { get; set; }
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Uživatelské jméno je povinné")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email je povinný")]
+        [EmailAddress(ErrorMessage = "Neplatný email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Heslo je povinné")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Heslo musí mít alespoò 6 znakù")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Hesla se neshodují")]
+        [Display(Name = "Potvrzení hesla")]
+        public string ConfirmPassword { get; set; }
+    }
 }
--- Source/EP_Kviz/Program.cs
+++ Source/EP_Kviz/Program.cs
@@ -45,6 +45,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllerRoute(
     name: "default",
