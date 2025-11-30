05086de8fbac27bf06c3766eb0b43ee39c2232de
Ondřej Imlauf <156407890+imlaufo23@users.noreply.github.com>
2025-11-30 20:07:19 +0100

    Přidání počet odehraných her a počet výher do databáze

--- Source/EP_Kviz/Models/UzivateleModel.cs
+++ Source/EP_Kviz/Models/UzivateleModel.cs
@@ -5,5 +5,11 @@ namespace EP_Kviz.Models
     public class UzivateleModel:IdentityUser
     {
 
+
+        /// Celkový počet her, které uživatel odehrál.
+        public int PocetOdehranychHer { get; set; } = 0;
+
+        /// Celkový počet her, které uživatel vyhrál.
+        public int PocetVyhranychHer { get; set; } = 0;
     }
 }
